41c4ebd5fb67d2d356d3eadd1c219a984654e25d
Ben Anderson <dataknut@icloud.com>
2019-01-14 17:02:56 +1300

    moved test code to testData.R

--- loadDataFromHCS.R
+++ loadDataFromHCS.R
@@ -17,13 +17,3 @@ df <- readr::read_csv(dFile)
 
 # quick look
 head(df)
-
-# Note that readr loaded it into a tbl - which has fancy printing
-
-table(df$day_of_week) # number of obs per day
-table(df$month) # number of obs per month
-
-# extract hour
-library(lubridate)
-df$hour <- lubridate::hour(df$time)
-table(df$hour) # number of observations per hour 
