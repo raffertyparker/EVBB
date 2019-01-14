8a575250944d30ee40cb631afb60b8130a914e14
Ben Anderson <dataknut@icloud.com>
2019-01-14 16:30:20 +1300

    added code to load data from HCS

---
+++ loadDataFromHCS.R
@@ -0,0 +1,29 @@
+# Loads data from Otago HCS
+library(readr)
+
+# Will only work if you have mounted the HCS as a volume (on Mac) or drive. This may require the VPN if you are off campus.
+
+# Adjust pathname for your platform
+
+# this works on a Mac:
+dPath <- "/Volumes/hum-csafe/Research Projects/GREEN Grid/Self_contained_Projects/2018_evChargingRafferty/data/"
+
+# use the same method to load from Dropbox - just change dPath, readr will still work provided it can access the file.
+
+# The data file:
+dFile <- paste0(dPath, "2019_12_10/EVBB_processed1.csv")
+
+df <- readr::read_csv(dFile)
+
+# quick look
+head(df)
+
+# Note that readr loaded it into a tbl - which has fancy printing
+
+table(df$day_of_week) # number of obs per day
+table(df$month) # number of obs per month
+
+# extract hour
+library(lubridate)
+df$hour <- lubridate::hour(df$time)
+table(df$hour) # number of observations per hour 
